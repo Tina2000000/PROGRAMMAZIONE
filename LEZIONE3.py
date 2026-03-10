3d5d9b50d9ce0959925a586c6a4ee292653972e8
Tina2000000 <VALENTINA.ZANOTTO@studenti.units.it>
2026-03-10 16:51:31 +0000

    lezione 3

---
+++ LEZIONE3.py
@@ -0,0 +1,16 @@
+def is_pari(n):
+    """ritorna vero se è pari, ristorna falso se è dispari """
+    risultato = False
+
+    if n%2 == 0
+       risultato= True
+    
+    
+    return risultato
+
+
+numero = int.input("Dammi un numero")
+print(type(numero))
+resault = is_pari(numero)
+
+print(resault)
