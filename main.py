ed9f496a75ba4b1d9ec70460fd28eff41c27fc6b
Tina2000000 <VALENTINA.ZANOTTO@studenti.units.it>
2026-03-15 11:09:13 +0000

    esercio 1 punto 1

---
+++ main.py
@@ -0,0 +1,14 @@
+#ESERCIZIO 1
+def is_pari(n):
+   '''controllo se il numero è pari'''
+
+   if n%2 == 0:
+      return True
+   else:
+      return False
+
+
+result = is_pari(4)
+print(result)
+result = is_pari(5)
+print(result)
