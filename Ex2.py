5973ba68640b842f9f0f6fe07819468091c0b002
Tina2000000 <VALENTINA.ZANOTTO@studenti.units.it>
2026-03-24 16:45:41 +0000

    Esercizi

---
+++ Ex2.py
@@ -0,0 +1,59 @@
+testo = '''
+Day after day, day after day,
+We stuck, nor breath nor motion;
+As idle as a painted ship
+Upon a painted ocean.
+
+Water, water, every where,
+And all the boards did shrink;
+Water, water, every where,
+Nor any drop to drink.
+
+The very deep did rot: O Christ!
+That ever this should be!
+Yea, slimy things did crawl with legs
+Upon the slimy sea.
+
+About, about, in reel and rout
+The death-fires danced at night;
+The water, like a witch's oils,
+Burnt green, and blue and white.
+'''
+# Risolvendo parte 1 esercizio 2:
+lista_righe = testo.split('\n')
+
+contatore = 0
+for riga in lista_righe:
+    if len(riga) > 0:
+        contatore = contatore + 1
+
+print(contatore)
+
+# Risolvendo parte 2 esercizio 2
+
+lista_parole = testo.split()
+
+contatore = 0
+for parola in lista_parole:
+    if len(parola) > 0:
+        contatore = contatore + 1
+
+print(contatore)
+
+
+
+# Risolvendo parte 3 esercizio 2
+
+lista_caratteri = testo.split()
+
+contatore = 0
+for carattere in lista_caratteri
+    if len(carattere)=1
+       contatore = contatore + 1
+
+# Risolvendo parte 8 esercizio 2
+
+
+
+
+
--- main.py
+++ main.py
@@ -12,3 +12,28 @@ result = is_pari(4)
 print(result)
 result = is_pari(5)
 print(result)
+#ESERCIZIO 1 punto 2
+def chiedi_numero_positivo():
+    while True:
+        '''crea ciclo infinito che si interrompe con return'''
+        try:
+            numero = int(input("Inserire un numero intero positivo"))
+            if numero > 0:
+             '''verifico che il numero sia maggiore di 0'''
+             return numero
+            else:
+                print("Errore : il numero deve essere maggiore di zero")
+        except ValueError :
+            print("Inserire un numero intero valido")
+            
+n = chiedi_numero_positivo()
+print("Hai inserito:", n)
+#ESERCIZIO 1 punto 3
+def genera_lista(n):
+    '''genera lista da n'''
+
+    if n%2 = 0:
+      return n%2
+   else
+      return (n*3)+1
+     
